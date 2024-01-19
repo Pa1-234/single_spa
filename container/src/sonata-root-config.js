584381a52fb927ebefcadf1421a06119557a6f54
nikshithas291993 <nikshitha.s@sonata-software.com>
2024-01-19 11:42:56 +0530

    update on loading page

--- container/src/sonata-root-config.js
+++ container/src/sonata-root-config.js
@@ -13,25 +13,7 @@ const applications = constructApplications({
     return System.import(name);
   },
 });
-registerApplication({
-  name: "@sonata/homepage",
-  app: () => System.import("@sonata/homepage"),
-  activeWhen: (location) => {
-  // Activate when the path is the main page or root ("/")
-    return location.pathname === "/" || location.pathname === "/homepage";
-  }, // Display on the main page
-  customProps: {
-    // Add custom props if needed
-  },
-});
-registerApplication({
-  name: "@sonata/footer",
-  app: () => System.import("@sonata/footer"),
-  activeWhen: ["/"], // Display footer on all pages
-  customProps: {
-    // Add custom props if needed
-  },
-});
+
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
